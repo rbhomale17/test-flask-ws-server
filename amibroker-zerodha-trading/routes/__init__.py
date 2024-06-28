40776252bc1b2fb265dfb28f37044be2f12d831a
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2024-06-28 11:51:05 +0530

    Update __init__.py

--- amibroker-zerodha-trading/routes/__init__.py
+++ amibroker-zerodha-trading/routes/__init__.py
@@ -4,6 +4,7 @@ from .profile import create_profile_blueprint
 from .gtt import create_gtt_blueprint
 # from .trades import create_trades_blueprint
 from .index import index_blueprint
+from .log import create_log_blueprint
 
 def register_blueprints(app, db, kite):
     app.register_blueprint(create_order_blueprint(db, kite))
@@ -12,3 +13,4 @@ def register_blueprints(app, db, kite):
     app.register_blueprint(create_gtt_blueprint(db, kite))
     # app.register_blueprint(create_trades_blueprint(db, kite))
     app.register_blueprint(index_blueprint(db, kite))
+    app.register_blueprint(create_log_blueprint(db,kite))
